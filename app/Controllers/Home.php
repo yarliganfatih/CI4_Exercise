c1fcaf81fa17f6e7df34a0f5b0c35702af2ae3a2
gobox BAUGO <gobox@bahce.bahcesehir.edu.tr>
2022-12-20 15:21:51 +0300

    layout template structure initial

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,9 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data = [
+            "pageTitle" => "Home Page"
+        ];
+        return view('home', $data);
     }
 }
